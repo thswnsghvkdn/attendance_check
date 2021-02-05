d117942e6a25a0a85edffbfb5cd183d9d8438dd2
thswnsghvkdn <ths3630802@naver.com>
2021-02-05 20:57:09 +0900

    no message

--- client/src/Att.js
+++ client/src/Att.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Excel from 'exceljs'
 
 
+
 // var data = wb.worksheets[2].columns[15].values
 /*function check() {
     var st = [1,0,1,0,1,0,1,0,1];
@@ -12,6 +13,38 @@ import Excel from 'exceljs'
     }
 }
 */
+
+
+
+
+function savefile( wb) // 서버에서 넘어온 객체를 가지고 엑셀을 수정 
+{
+  var res ={ att:
+    [ [ 1, null, 1, 1 ],
+      [ null, 1, null, 1 ],
+      [ null, null, 1, 1 ],
+      [ 1, null, 1, 1 ],
+      [ null, 1, null, 1 ],
+      [ 1, null, 1, 1 ] ],
+   date: [ '2-1', '2-0', '1-3', '1-2', '1-1', '1-0' ] };
+    for(var i = 0 ; i < res.date.length ; i++)
+    {
+        var date = res.date[i]; // '2-1' 과 같이 월과 주가 객체로 전달된다.
+        var m = Number(date[0])
+        var c = Number(date[2])
+        for(var j = 0 ; j < res.att.length ; j++){
+            {
+                for(var k = 0 ; k < res.att[j].length ; k++){
+                    if(res.att[j][k] === 1){
+                      wb.worksheets[m + 2].getRow(5 + k).getCell(16 + c).value = 1;
+                      debugger;
+                     }
+                }
+            }
+        }
+    }
+}
+
 
 class Att extends React.Component {
 state = {file : null}
@@ -27,32 +60,33 @@ fileHandler = (e) => {
     reader.onload = () => {
       const buffer = reader.result;
       wb.xlsx.load(buffer).then(data => {
-          debugger;
-          console.log(wb);
-          var st = [1,0,1,0,1,0,1,0,1];
-          for(var i = 0 ; i < st.length ; i++){
-              if(st[i] == 1){
-               wb.worksheets[3].getRow(i + 5).getCell(16).value = 1;
-              }
-          }
-          debugger;
+          
+        savefile( wb); // 받아온 엑셀에 수정하기
+
           wb.xlsx.writeBuffer().then(function (data) {
             const blob = new Blob([data],
               { type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' });
             const url = window.URL.createObjectURL(blob);
             const anchor = document.createElement('a');
             anchor.href = url;
-            anchor.download = 'download.xls';
+            anchor.download = 'download.xlsx';
             anchor.click();
             window.URL.revokeObjectURL(url);
           });
       })
     }
   }
+  date = (e) =>{
+      const d = e.target.date;
+      debugger;
+  }
+  
+
   render() {
     return (
       <div className="Att">
         <input type="file" multiple onChange={this.fileHandler}/>
+        <input type = "date" id = "myDate" multiple onChage={this.date} />
       </div>
     );
   }
--- server.js
+++ server.js
@@ -6,7 +6,10 @@ const excel = require('exceljs')
 const dbuser = require('./config')
 const cell = 5
 
+const month = new Date().getMonth() 
+const week = parseInt(new Date().getDate() / 7)
 
+var obj = {att : [] , date : []}
 
 app.use(bodyparser.json())
 
@@ -21,10 +24,44 @@ var conn = mysql.createConnection({
 conn.connect( (err) =>{
     if(err)
         console.log(err)
-    else 
+    else {
         console.log('DB connected')
+        tt()
+    }
 })
 
+function recursive(m , w)
+{
+
+    var _date =   m + '-' + w ;
+    var str = 'select `'+ _date + '` from attendance'
+    conn.query(str ,function(err, result){
+        if(result) {
+            var t = []
+            for(var j = 0 ; j < result.length ; j++)
+                t.push(result[j][_date])
+            obj.att.push(t)
+            obj.date.push(_date)
+            if(m == 1 && w == 0) console.log(obj)
+        }
+    })
+}
+
+function tt()
+{
+    m = month + 1;
+    w = week * 2 + 1;
+    while(m >= 1)
+    {
+        recursive(m , w, obj)
+        w--;
+        if(w < 0)
+        {
+            m--;
+            w = 9;
+        }
+    }
+}
 
 var students = '준호 채윤 훈모'
 var new_stu = students.split(' ')
@@ -45,33 +82,49 @@ function att() {
     }
 }
 
-function save(sheet , cell) {
-
-
-
-    while(1)
-    {
-        var index
-        var check_cell =  year + '-' + start_sheet + '-' + start_cell // 다음달로 넘어가기 위해
-        conn.query(check_cell, function(err, result){
-            if(!result) {
-                start_sheet++;
-                start_cell = 16    
-            }
-        })    
-        index = year + '-' + start_sheet + '-' + start_cell
-        var str = 'select * from attendance where name like' + conn.escape('%' + new_stu[i] +'%')
-        conn.query(str , function(err, result){
-            if(err) ( err => console.log(err))
-            console.log(result)
-        })
-    }
-    
-
+function get_column(){
+    var str = 'select name from attendance'
+    conn.query(str, function(err, result){
+        debugger;
+        console.log(result)
+        console.log(result[0].name)
+        
+    })
 }
 
-att()
-app.get('/attendance' , (req, res) => {
+function save() {
+
+    debugger;
+    var m = month + 2; // worksheet 번호
+    var w = week * 2 + 1;
+    var att = []
+    var date = []
+    while( m <= 1 ) {
+        var _date = '`' +  m + '-' + w + '`';
+        var str = 'select '+ _date +' from attendance'
+        console.log(str)
+        conn.query(str , function(err, result){
+            if(result) {
+                console.log(result)
+                if(err) ( err => console.log(err))
+                att.push(result[_date])
+            }
+        })
+        // 다음 컬럼을 받는다. 
+        w--;
+        if(w < 0) // 이전 달로 이동
+        {
+            w = 9;
+            m--
+        }
+        date.push(_date)
+    }
+    console.log({ att : att , date : date});
+    debugger;
+    return({ att : att , date : date});
+    
+}
+app.post('/attendance' , (req, res) => {
     //var students = req.body.students;
 
     var students = '준호 채윤 훈모'
@@ -84,7 +137,34 @@ app.get('/attendance' , (req, res) => {
             console.log(result)
         })
     }
-
 })
+
+app.get('/save', (req, res) =>{
+    var m = month + 2; // worksheet 번호
+    var w = week * 2 + 1;
+    var att = []
+    var date = []
+    while( m <= 1 ) {
+        var _date = '`' +  m + '-' + w + '`';
+        var str = 'select '+ _date +' from attendance'
+        conn.query(str , function(err, result){
+            if(result) {
+                if(err) ( err => console.log(err))
+                att.push(result[_date])
+            }
+        })
+        // 다음 컬럼을 받는다. 
+        w--;
+        if(w < 0) // 이전 달로 이동
+        {
+            w = 9;
+            m--
+        }
+        date.push(_date)
+    }
+    res.send({ att : att , date : date});
+})
+
+
 
 app.listen(3000, () => console.log('sever is running at port no 3000'));
